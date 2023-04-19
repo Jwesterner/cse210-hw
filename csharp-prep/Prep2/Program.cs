8c43bebe3942c92236b9069e9838daa706e3c369
Jwesterner <jwesterner@gmail.com>
2023-04-19 15:36:31 -0600

    completed Prep1 & Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char letter;
+        int grade;
+        string passFail;
+
+        Console.WriteLine("Please enter your grade: ");
+        grade = Convert.ToInt32(Console.ReadLine());
+
+        if (grade>=70) {
+            passFail = "You are passing your class! Nice!";
+        }
+        else {
+            passFail = "Work a bit harder and you might pass!";
+        }
+
+        if (grade>=90) {
+            letter = 'A';
+        }
+        else if (grade>=80 && grade<90) {
+            letter = 'B';
+        }
+        else if (grade>=70 && grade<80) {
+            letter = 'C';
+        }
+        else if (grade>=60 && grade<70) {
+            letter = 'D';
+        }
+        else {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}. {passFail}");
+
     }
 }
