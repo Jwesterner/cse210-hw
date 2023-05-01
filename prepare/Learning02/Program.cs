87e284c7cb8a4c55d886a945de6ddb78efc5b7a8
Jwesterner <jwesterner@gmail.com>
2023-05-01 12:19:32 -0600

    Completed Learning02 Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
+using System.Threading.Tasks.Dataflow;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Declare objects.
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Resume resume = new Resume();
+
+        // Add information to Job objects.
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Add information to resume object.
+        resume._name = "Jack West";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Diplay resume contents to terminal.
+        resume.Display();
+
+        
+
     }
 }
