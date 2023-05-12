cd8e405a62f365eb9935cd6540da590ca293b079
Jwesterner <jwesterner@gmail.com>
2023-05-12 17:18:14 -0600

    added menu and finished program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,23 +1,65 @@
+using System.Globalization;
 using System;
 using System.Collections.Generic;
 
 namespace Develop02 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Entry myEntry = new Entry();
-            myEntry.store("What is your name?", "Jack West", "8 May 2023");
-
-            Journal journal = new Journal();
-            journal.StoreEntry(myEntry);
-
-            List<Entry> entries = journal.GetAllEntries();
+    class Program {
+        static void Main(string[] args) {
             
-            foreach (Entry entry in entries) {
-                string message = entry.GetAsString();
-                Console.WriteLine(message);
+            DataStore ds = new DataStore();
+            Journal journal = new Journal();
+            Prompt prompts = new Prompt();
+
+
+            string current_date = DateTime.Now.ToShortDateString();
+            string response;
+            string random_prompt;
+            string file;
+            int choice;
+
+            Console.WriteLine("Welcome to the Journal Program!");
+            do {
+                Console.WriteLine("Please select on of the following choices");
+                Console.WriteLine("1) Write\n2) Display\n3) Load\n4) Save\n5) Quit\n");
+                choice = Convert.ToInt16(Console.ReadLine());
+
+                if (choice == 1) {
+                    random_prompt = prompts.GetRandomString();
+                    Console.WriteLine(random_prompt);
+                    response = Console.ReadLine();
+
+                    journal.StoreEntry(new Entry() {
+                        date = current_date,
+                        prompt = random_prompt,
+                        response = response
+                    });
+                }
+
+                if (choice == 2) {
+                    foreach (Entry item in journal.GetAllEntries()) {
+                        Console.WriteLine(item.GetAsString());
+                    }
+                }
+
+                if (choice == 3) {
+                    Console.WriteLine("Enter the name of the file you would like to load: ");
+                    file = Console.ReadLine();
+                    ds.LoadFile(file, journal);
+                }
+
+                if (choice == 4) {
+                    Console.WriteLine("Enter the name of the file you would like to save to: ");
+                    file = Console.ReadLine();
+                    ds.SaveToFile(file, journal.GetAllEntries());
+                }
+
+
+            } while (choice != 5);
+
+            
+
+
+
             }
         }
     }
-}
