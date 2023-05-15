ff6dbd17e39a90c0e48365b53b51829d2c271491
Jwesterner <jwesterner@gmail.com>
2023-05-15 15:30:27 -0600

    created fraction class

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,48 @@
+
+namespace Learning03 {
+
+    public class Fraction {
+
+        private int _top;
+        private int _bottom;
+
+        public Fraction() {
+            _top = 1;
+            _bottom = 1;
+        }
+
+        public Fraction(int wholeNumber) {
+            _top = wholeNumber;
+            _bottom = 1;
+        }
+
+        public Fraction(int top, int bottom) {
+            _top = top;
+            _bottom = bottom;
+        }
+
+        public int getTop() {
+            return _top;
+        }
+
+        public int getBottom() {
+            return _bottom;
+        }
+
+        public void setTop(int top) {
+            _top = top;
+        }
+
+        public void setBottom(int bottom) {
+            _bottom = bottom;
+        }
+
+        public string getFractionString() {
+            return $"{_top}/{_bottom}";
+        }
+
+        public double getDecimalValue() {
+            return (double) _top / (double) _bottom;
+        }
+    }
+}
