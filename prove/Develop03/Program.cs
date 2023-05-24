c8fcaec437af5e174e940c5232484b8bd0900df3
Jwesterner <jwesterner@gmail.com>
2023-05-24 13:26:55 -0600

    added new class files

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,11 @@
 using System;
 
+namespace Develop03 {
 class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Develop03 World!");
+        }
     }
 }
