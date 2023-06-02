4cde2ed9895625bbca5ad3cbc58c20c507519212
Jwesterner <jwesterner@gmail.com>
2023-06-02 16:14:54 -0600

    add test cases

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
+namespace Learning05 {
 class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+            Sqaure sq = new Sqaure("Red", 2.5);
+            Rectangle rc = new Rectangle("Blue", 4.5, 2.3);
+            Circle cr = new Circle("Yellow", 2.7);
+
+            shapes.Add(sq);
+            shapes.Add(rc);
+            shapes.Add(cr);
+
+            foreach (Shape shape in shapes) {
+                Console.WriteLine(shape.GetArea());
+            }
+        }
     }
 }
