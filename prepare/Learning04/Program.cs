3b350bd0fe7df6ad31b609b9cfd72170828f6eb5
Jwesterner <jwesterner@gmail.com>
2023-06-02 13:17:25 -0600

    added test cases

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Learning04 {
+
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
+        static void Main(string[] args)
+        {
+            Assignment assignment1 = new Assignment("Samuel Bennet", "Multiplication");
+
+            MathAssignemnt mathAssignemnt1 = new MathAssignemnt("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+            WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+            Console.WriteLine(assignment1.GetSummary());
+
+            Console.WriteLine(mathAssignemnt1.GetSummary());
+            Console.WriteLine(mathAssignemnt1.GetHomeworkList());
+
+            Console.WriteLine(writingAssignment1.GetSummary());
+            Console.WriteLine(writingAssignment1.GetWritingInformation());
+        }
     }
 }
